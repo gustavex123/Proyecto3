9beaf7c6f927abca4e974f8fa4b5911ebd605dd0
Gustavex <tavex.ac@gmail.com>
2026-04-27 10:03:16 -0400

    actualizacion
    actualización

--- src/infrastructure/database/mongo/note.mongo.repository.js
+++ src/infrastructure/database/mongo/note.mongo.repository.js
@@ -17,4 +17,33 @@ export default class NoteMongoRepository {
     async findByUserId(userId) {
        return await NoteModel.find({ userId });
     }
+
+    async getById(id) {
+    const note = await NoteModel.findById(id);
+    if (!note) return null;
+    return note.toObject();
+    }
+
+    async update(id, data) {
+        const updatedNote = await NoteModel.findByIdAndUpdate(
+            id,
+            {
+                title: data.title,
+                content: data.content,
+                imageUrl: data.imageUrl,
+                isPrivate: data.isPrivate,
+                password: data.password
+            },
+            { new: true } // devuelve el actualizado
+        );
+
+        if (!updatedNote) return null;
+        return updatedNote.toObject();
+    }
+
+    async delete(id) {
+        const deletedNote = await NoteModel.findByIdAndDelete(id);
+        if (!deletedNote) return null;
+        return deletedNote.toObject();
+    }
 }
